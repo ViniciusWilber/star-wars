16be0b656334d03b004f74b3782c329ca5cc1285
ViniciusWilber <viniwilberrr@gmail.com>
2023-09-05 20:00:25 -0300

    adicionamos as funcoes do js e os cards individuais

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,7 +1,32 @@
 $.ajax({
     type: 'GET',
     url: 'https://swapi.dev/api/people/',
-    success: function(response) {
-      console.log(response);
+    success:function(response) {
+      mostraDadosNaTela(response);
     }
 });
+
+var listaPersonagens=[];
+
+
+function mostraDadosNaTela(response) {
+
+  for(i=0; i<response.results.length; i++){
+    listaPersonagens.push(response.results[i]);
+  }
+   criaCardPersonagens(listaPersonagens);
+}
+
+function criaCardPersonagens(lista){
+
+  for(i=0; i<lista.length; i++){
+
+    criaCardPersonagem(lista[i]);
+  }
+}
+
+function criaCardPersonagem(personagem){
+
+  console.log("estou criando um pesonagem");
+  console.log(personagem);
+}
