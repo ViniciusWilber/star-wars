7e40bdfed93659b72ffef8e65aedc458e08308fd
ViniciusWilber <viniwilberrr@gmail.com>
2023-10-05 19:57:15 -0300

    inserimos as novas imagens e definimos a continuação do projeto, como adicionar a proxima pagina

--- js/funcoes.js
+++ js/funcoes.js
@@ -7,7 +7,7 @@ $.ajax({
 });
 
 var listaPersonagens=[];
-var listaImagens=["../imagens/luke_the_hero.webp","../imagens/C3poBusto.jpg","../imagens/luke_the_hero.webp","../imagens/C3poBusto.jpg","../imagens/luke_the_hero.webp","../imagens/C3poBusto.jpg","../imagens/luke_the_hero.webp","../imagens/C3poBusto.jpg","../imagens/luke_the_hero.webp","../imagens/C3poBusto.jpg"]
+var listaImagens=["../imagens/luke_the_hero.webp","../imagens/C3poBusto.jpg","../imagens/R2D2Busto.jpg","../imagens/dartvaderBusto.jpg","../imagens/LeiaOrgana.webp","../imagens/OwenLars.jpg","../imagens/BeruWhitesun.jpg","../imagens/R5D4.jpg","../imagens/BiggsDarklighter.webp","../imagens/Obiwankenobi.webp"]
 
 
 function mostraDadosNaTela(response) {
@@ -29,7 +29,7 @@ var elementoCards=$("#cards");
 function criaCardPersonagem(personagem,imagem){
 elementoCards.append(`<div class="col-sm-3">
 <div class="card mx-auto my-4 bg-dark" style="width: 18rem;">
-  <img src= ${imagem} class="card-img-top" alt="...">
+  <img src= ${imagem} class="card-img-top alturaCard" alt="...">
   <div class="card-body">
     <p class="card-text text-light text-center">${personagem.name}</p>
   </div>
